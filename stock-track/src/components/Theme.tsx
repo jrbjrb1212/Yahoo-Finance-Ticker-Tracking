2823115bbce7c1d9addec29b28eff3e79228d2d2
jrbjrb1212 <77088661+jrbjrb1212@users.noreply.github.com>
2024-12-22 00:37:30 -0800

    common theme

---
+++ stock-track/src/components/Theme.tsx
@@ -0,0 +1,12 @@
+// src/theme.ts
+const Theme = {
+  colors: {
+    primary: "#8354e0", // Example primary color
+    secondary: "#F9FCFF", // Example secondary color
+    background: "#f4f4f4", // Background color
+    outline: "#E0E0E0",
+    text: "#333333", // Text color
+  },
+};
+
+export default Theme;
